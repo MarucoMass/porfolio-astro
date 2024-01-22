f632a16b4ffce1f0e9482bdda4713a12f7ca256c
Mario Massonnat <mariomass23@gmail.com>
2024-01-22 02:17:59 -0300

    added bento grid to experience item

---
+++ src/components/data/educative.js
@@ -0,0 +1,21 @@
+export const EDUCATIVE = [
+  {
+    date: "Junio 2023 - Actualidad",
+    title: "Tecnicatura Universitaria en Programación",
+    company:"Universidad Tecnológica Nacional",
+    description:
+      "Divulgo sobre programación y desarrollo web en diferentes plataformas. Galardonado como mejor creador de contenido de habla no-inglesa en 2022 y mejor comunidad en 2023 por GitHub.",
+    link: "https://junior.org.ar/", 
+    img: "/junior.webp"
+  },
+  {
+    date: "Diciembre 2022 - Actualidad",
+    title: "Web Developer & Business Developer",
+    company:"Efecto Digital LLC",
+    description:
+      "Responsable de la plataforma, componentes y utilidades para la creación y desarrollo de aplicaciones web. Mejora de un 30% en la entrega de software. Implantación de medidas de integración continua y despliegue con A/B testing en más de 15 equipos.",
+    link:"https://efectodigital.com.ar/", 
+    img:"/efecto.webp"
+  },
+  ]
+  
