d9418eb1e0fb33ea44ca023c974dc61ad79dbcb5
Mario Massonnat <mariomass23@gmail.com>
2024-01-23 04:08:34 -0300

    added img in projects section

---
+++ src/components/data/projects.js
@@ -0,0 +1,102 @@
+export const PROJECTS = [
+    {
+      title: "Fintelligence",
+      technologies:"html, css, javascript",
+      description:
+        "Divulgo sobre programación y desarrollo web en diferentes plataformas. Galardonado como mejor creador de contenido de habla no-inglesa en 2022 y mejor comunidad en 2023 por GitHub.",
+      repository: "https://junior.org.ar/", 
+      deployment: "https://junior.org.ar/", 
+      img: "../../../public/assets/fintelligence.webp"
+    },
+    {
+      title: "Plantilla de Landing Page",
+      technologies:"html, css, javascript",
+      description:
+        "Divulgo sobre programación y desarrollo web en diferentes plataformas. Galardonado como mejor creador de contenido de habla no-inglesa en 2022 y mejor comunidad en 2023 por GitHub.",
+      repository: "https://junior.org.ar/", 
+      deployment: "https://junior.org.ar/", 
+      img: "../../../public/assets/plantilla.webp"
+    },
+    {
+      title: "Prospectiva",
+      technologies:"html, css, javascript",
+      description:
+        "Divulgo sobre programación y desarrollo web en diferentes plataformas. Galardonado como mejor creador de contenido de habla no-inglesa en 2022 y mejor comunidad en 2023 por GitHub.",
+      repository: "https://junior.org.ar/", 
+      deployment: "https://junior.org.ar/", 
+      img: "../../../public/assets/prospectiva.webp"
+    },
+    {
+      title: "Rpa-Works",
+      technologies:"html, css, javascript",
+      description:
+        "Divulgo sobre programación y desarrollo web en diferentes plataformas. Galardonado como mejor creador de contenido de habla no-inglesa en 2022 y mejor comunidad en 2023 por GitHub.",
+      repository: "https://junior.org.ar/", 
+      deployment: "https://junior.org.ar/", 
+      img: "../../../public/assets/rpa-works.webp"
+    },
+    {
+      title: "Flor de Irupé",
+      technologies:"html, css, javascript",
+      description:
+        "Divulgo sobre programación y desarrollo web en diferentes plataformas. Galardonado como mejor creador de contenido de habla no-inglesa en 2022 y mejor comunidad en 2023 por GitHub.",
+      repository: "https://junior.org.ar/", 
+      deployment: "https://junior.org.ar/", 
+      img: "../../../public/assets/irupe.webp"
+    },
+    {
+      title: "Frente Patria",
+      technologies:"html, css, javascript",
+      description:
+        "Divulgo sobre programación y desarrollo web en diferentes plataformas. Galardonado como mejor creador de contenido de habla no-inglesa en 2022 y mejor comunidad en 2023 por GitHub.",
+      repository: "https://junior.org.ar/", 
+      deployment: "https://junior.org.ar/", 
+      img: "../../../public/assets/frentepatria.webp"
+    },
+    {
+      title: "Pedal Store",
+      technologies:"html, css, javascript",
+      description:
+        "Divulgo sobre programación y desarrollo web en diferentes plataformas. Galardonado como mejor creador de contenido de habla no-inglesa en 2022 y mejor comunidad en 2023 por GitHub.",
+      repository: "https://junior.org.ar/", 
+      deployment: "https://junior.org.ar/", 
+      img: "../../../public/assets/pedalstore.webp"
+    },
+    {
+      title: "Musicasa",
+      technologies:"html, css, javascript",
+      description:
+        "Divulgo sobre programación y desarrollo web en diferentes plataformas. Galardonado como mejor creador de contenido de habla no-inglesa en 2022 y mejor comunidad en 2023 por GitHub.",
+      repository: "https://junior.org.ar/", 
+      deployment: "https://junior.org.ar/", 
+      img: "../../../public/assets/musicasa.webp"
+    },
+    {
+      title: "Caida Libre",
+      technologies:"html, css, javascript",
+      description:
+        "Divulgo sobre programación y desarrollo web en diferentes plataformas. Galardonado como mejor creador de contenido de habla no-inglesa en 2022 y mejor comunidad en 2023 por GitHub.",
+      repository: "https://junior.org.ar/", 
+      deployment: "https://junior.org.ar/", 
+      img: "../../../public/assets/caidalibre.webp"
+    },
+    {
+      title: "Tira Tiro",
+      technologies:"html, css, javascript",
+      description:
+        "Divulgo sobre programación y desarrollo web en diferentes plataformas. Galardonado como mejor creador de contenido de habla no-inglesa en 2022 y mejor comunidad en 2023 por GitHub.",
+      repository: "https://junior.org.ar/", 
+      deployment: "https://junior.org.ar/", 
+      img: "../../../public/assets/tiratiro.webp"
+    },
+    {
+      title: "Hasta Las Muelas",
+      technologies:"html, css, javascript",
+      description:
+        "Divulgo sobre programación y desarrollo web en diferentes plataformas. Galardonado como mejor creador de contenido de habla no-inglesa en 2022 y mejor comunidad en 2023 por GitHub.",
+      repository: "https://junior.org.ar/", 
+      deployment: "https://junior.org.ar/", 
+      img: "../../../public/assets/hastalasmuelas.webp"
+    },
+  
+  ]
